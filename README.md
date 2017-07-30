e8a8c2841024eea03de5feec3d0e61d3d9d6c7c2
albertc <albertc@loftusit.com.au>
2017-07-30 16:49:27 +0930

    Fixing bug and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # postfix
+
+Assumptions:
+1.  System data source is embedded and displayed when the program is executed
+2.  Java minium version is 1.8
+3.  Apache Maven 3.2.2 used to build the jar file
+
+Implementation details:
+The program is executable using Java 1.8.  No other third-party library was used.
+To run the program, generate the jar file first using maven: 
+<code>mvn clean install -DskipTests</code>
+
+Navigate to the target directory and execute the program:
+```
+java -jar postfix-1.0.jar /tmp/input.txt /tmp/output.txt
+```
+
+Limitations:
+1.  Current data source only support columns A to C and rows 1 to 5 only (A1..C5)
+<br>
+| A  | B | C |
+|--- |---|---|
+|1 |6|11|
+|2 |7|12|
+|3 |8|13|
+|4 |9|14|
+|5 |10|15|
+<br>
+2.  CSV input file supports cell value of whole number only
--- src/main/java/com/ahpc/PostfixEvaluator.java
+++ src/main/java/com/ahpc/PostfixEvaluator.java
@@ -23,39 +23,9 @@ import java.util.regex.Pattern;
 public class PostfixEvaluator {
 	private static final String VALID_OPERAND = "-|\\+|/|\\*";
 	private static final String VALID_COL_ROW = "[a-cA-C][1-5]";
-
-	public static void main(String... args) {
-		if (args == null || args.length != 2) {
-			System.out.println("Please enter source and output filenames");
-			throw new IllegalArgumentException("Invalid parameter");
-		}
-		String inputFile = args[0];
-		String outputFile = args[1];
-		String linePostfix = "";
-		Path p = Paths.get(outputFile);
-		try (Scanner s = new Scanner(new FileInputStream(inputFile));
-				BufferedWriter bw = Files.newBufferedWriter(p, StandardCharsets.UTF_8);) {
-			String rows;
-			
-			while (s.hasNextLine()) {
-				rows = s.nextLine();
-				linePostfix = evaluateLine(rows);
-				bw.write(linePostfix);
-				bw.newLine();
-//				System.out.format("Line: %s \n", rows);
-//				System.out.format("linePostfix: %s \n", linePostfix);
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Please enter a valid source file");
-			throw new IllegalArgumentException("Invalid source file");
-		} catch (IOException e) {
-			System.out.println("Error in writing to output file");
-			e.printStackTrace();
-		}
-	}
-
-	private static Map<String, List<Integer>> inputData() {
-		Map<String, List<Integer>> data = new HashMap<String, List<Integer>>();
+	private static Map<String, List<Integer>> data;
+	static {
+		data = new HashMap<String, List<Integer>>();
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(1);
 		list.add(2);
@@ -79,7 +49,40 @@ public class PostfixEvaluator {
 		list.add(14);
 		list.add(15);
 		data.put("c", list);
-		return data;
+	}
+
+	public static void main(String... args) {
+		if (args == null || args.length != 2) {
+			System.out.println("Please enter source and output filenames");
+			throw new IllegalArgumentException("Invalid parameter");
+		}
+		
+		String inputFile = args[0];
+		String outputFile = args[1];
+		String linePostfix;
+		System.out.format("Start processing postfix function for %s \n", inputFile);
+		System.out.format("Data source: %s \n", data);
+		Path p = Paths.get(outputFile);
+		try (Scanner s = new Scanner(new FileInputStream(inputFile));
+				BufferedWriter bw = Files.newBufferedWriter(p, StandardCharsets.UTF_8);) {
+			String rows;
+			
+			while (s.hasNextLine()) {
+				rows = s.nextLine();
+				linePostfix = evaluateLine(rows);
+				bw.write(linePostfix);
+				bw.newLine();
+				System.out.format("Line: %s \n", rows);
+				System.out.format("linePostfix: %s \n\n", linePostfix);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Please enter a valid source file");
+			throw new IllegalArgumentException("Invalid source file");
+		} catch (IOException e) {
+			System.out.println("Error in writing to output file");
+			e.printStackTrace();
+		}
+		System.out.format("Done, output written to %s", outputFile);
 	}
 
 	private static String evaluateLine(String line) {
@@ -88,10 +91,9 @@ public class PostfixEvaluator {
 			cells.useDelimiter(",");
 			while (cells.hasNext()) {
 				String cell = cells.next();
-				Float postfix = cell.trim().length() == 0 ? null : evaluateCell(cell);
-				result += postfix == null ? "#ERR" : postfix;
+				Float postfix = cell.trim().length() == 0 ? Float.valueOf(0) : evaluateCell(cell);
+				result += postfix == null ? "#ERR" : getValueAsString(postfix);
 				result += cells.hasNext() ? "," : "";
-				result = result.replace(".0", "");
 			}
 		}
 
@@ -130,7 +132,7 @@ public class PostfixEvaluator {
 						}
 					} else {
 						return null;
-					}
+					} 
 				}
 			}
 		}
@@ -144,7 +146,6 @@ public class PostfixEvaluator {
 	 * @return
 	 */
 	private static int getData(String colRow) {
-		Map<String, List<Integer>> data = inputData();
 		String column = colRow.substring(0, 1);
 		String row = colRow.substring(1, 2);
 		List<Integer> list = data.get(column.toLowerCase());
@@ -163,6 +164,12 @@ public class PostfixEvaluator {
 		Pattern p = Pattern.compile(pattern);
 		Matcher matcher = p.matcher(value);
 		result = matcher.matches();
+		return result;
+	}
+	
+	private static String getValueAsString(Float f) {
+		String result = (int) Math.ceil(f.doubleValue()) == f.intValue() ?  String.valueOf(f.intValue()) : String.valueOf(f);
+		
 		return result;
 	}
 }
